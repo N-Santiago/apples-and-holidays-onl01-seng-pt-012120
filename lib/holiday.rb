2ea0c261729370e5de94c33fda6f97e832beed25
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-02 04:21:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday_hash.each do |holiday, item|
-      puts " #{holiday.to_s.split"
+      puts " #{holiday.to_s.split("_").map"
 end
 
 def all_holidays_with_bbq(holiday_hash)
